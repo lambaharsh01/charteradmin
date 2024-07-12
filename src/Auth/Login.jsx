0749913fddfbc376bb348a748aa8f9914b798c7a
Harsh <118607392+lambaharsh01@users.noreply.github.com>
2024-07-12 07:13:41 +0530

    second commit

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -13,17 +13,24 @@ useEffect(()=>{
 }, []);
 
 const style={
-    mainDiv:{height:pageHeight, width:'100%', display:'flex',  justifyContent:'center', position:'absolute'},
-    centralDiv:{}
+    mainDiv:{height:pageHeight, width:'100%'},
+    centralDiv:{},
+    imgStyle:{height:150, width:150}
 }
 
 
 
     return(
-        <div style={style.mainDiv}>
-            <div style={{width:'50%', backgroundColor:'green', display:'flex',  justifyContent:'center'}}>
-                <img src={charterIcon}/>
-                Hoe
+        <div style={style.mainDiv} className="container-fluid d-flex justify-content-center">
+            <div className="row w-100 ">
+
+                <div className="col-10 col-sm-8 col-md-6 offset-1 offset-sm-2 offset-md-3 p-2 text-center" style={{backgroundColor:'green'}}>
+                <img src={charterIcon} style={style.imgStyle} className="m-auto"/>
+                <br/>
+                <input className="w-75 p-1 p-md-2 rounded-md"/>
+
+                </div>
+
             </div>
         </div>
     )
